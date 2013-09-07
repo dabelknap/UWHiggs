6d90a6883cd2c588529b2e475ad7992314bab852
Austin Belknap <dabelknap@wisc.edu>
2013-09-07 11:41:11 -0500

    Take channel and input files as arguments

--- 4l/SpinParity/mktemplates1D.py
+++ 4l/SpinParity/mktemplates1D.py
@@ -5,16 +5,27 @@ import numpy as np
 from numpy import cos
 import sys
 
-sys.argv.append( '-b-' )
+
+'''
+NTUPLE = "../ntuples/ggH125.h5"
+CHAN = "EEEE"
+'''
+if len(sys.argv) < 4:
+    sys.stderr.write('Usage: python mktemplates1D.py [HDF5 file] [channel] [outfile]\n')
+    sys.exit(1)
+
+NTUPLE = sys.argv[1]
+CHAN = sys.argv[2]
+OUTFILE = sys.argv[3]
+
+sys.argv.append('-b-')
 import ROOT as rt
 
-NTUPLE = "../ntuples/ggH125.h5"
-
-CHAN = "MMMM"
 
 CUTS = "(105.7 < mass) & (mass < 140.7)"
 
 NBINS = 8
+LUMI = 19.712
 
 
 def P2(x):
@@ -25,28 +36,35 @@ if __name__ == "__main__":
 
     h5file = tb.open_file(NTUPLE, 'r')
 
-    outfile = rt.TFile(CHAN + "_template.root", "RECREATE");
+    XSEC = float(h5file.root._v_attrs.XSEC)
+    NEVENTS = float(h5file.root._v_attrs.GENEVENTS)
+
+    outfile = rt.TFile(OUTFILE, "RECREATE")
 
     table = getattr(h5file.root, CHAN).events
 
     template = rt.TH3F("template", "template",
-            NBINS, -0.5, 1.0,  # P2(costheta1)
-            NBINS, -0.5, 1.0,  # P2(costheta2)
-            NBINS, -1.0, 1.0)  # cos(2Phi)
+                       NBINS, -0.5, 1.0,  # P2(costheta1)
+                       NBINS, -0.5, 1.0,  # P2(costheta2)
+                       NBINS, -1.0, 1.0)  # cos(2Phi)
 
     for x in table.where(CUTS):
-        template.Fill(P2(x['costheta1']), P2(x['costheta2']), cos(2.0*x['Phi']))
+        template.Fill(P2(x['costheta1']),
+                      P2(x['costheta2']),
+                      cos(2.0*x['Phi']),
+                      LUMI*XSEC/NEVENTS)
 
-    template_1d = rt.TH1F("template_1d","template_1d", NBINS**3, 0, 1)
+    template_1d = rt.TH1F("template_1d", "template_1d", NBINS**3, 0, 1)
 
     for i in xrange(1, NBINS+1):
         for j in xrange(1, NBINS+1):
             for k in xrange(1, NBINS+1):
                 ind_3d = template.GetBin(k, j, i)
                 ind_1d = template_1d.GetBin(k + (j-1)*NBINS + (i-1)*NBINS**2)
-                template_1d.SetBinContent(
-                        ind_1d,
-                        template.GetBinContent(ind_3d))
+                template_1d.SetBinContent(ind_1d,
+                                          template.GetBinContent(ind_3d))
+
+    print "Yield: ", template_1d.Integral()
 
     template_1d.Write()
 
