eb9014d19113429e7bf0c92d7c9df68169ae7d36
Austin Belknap <dabelknap@wisc.edu>
2014-01-22 08:46:25 -0600

    Add 4mu analyzer and base selections

---
+++ dblh/DblHAnalyzeMMMM.py
@@ -0,0 +1,68 @@
+import ROOT as rt
+import DblHBaseSelections as selections
+import MuMuMuMuTree
+from FinalStateAnalysis.PlotTools.MegaBase import MegaBase
+
+
+class DblHAnalyzeMMMM(MegaBase):
+    tree = 'mmmm/final/Ntuple'
+    def __init__(self, tree, outfile, **kwargs):
+        super(DblHAnalyzeMMMM, self).__init__(tree, outfile, **kwargs)
+        self.tree = MuMuMuMuTree.MuMuMuMuTree(tree)
+        self.out = outfile
+        self.histograms = {}
+        self.histo_locations = {}
+
+
+    def book_histos(self, folder):
+        self.book(folder, "m1Pt", "Muon Pt", 100, 0, 100)
+        self.book(folder, "m2Pt", "Muon Pt", 100, 0, 100)
+        self.book(folder, "m3Pt", "Muon Pt", 100, 0, 100)
+        self.book(folder, "m4Pt", "Muon Pt", 100, 0, 100)
+
+
+    def begin(self):
+        folder = 'test'
+        self.book_histos(folder)
+
+        for key in self.histograms:
+            location, name = key.split('/')
+            self.histo_locations.setdefault(location, []).append(name)
+
+
+    def fill_histos(self, histos, folder_str, row, weight=None):
+        for name in self.histo_locations[folder_str]:
+            print folder_str + '/' + name
+            value = self.histograms[folder_str + '/' + name]
+            if value.InheritsFrom('TH2'):
+                pass
+            else:
+                if weight:
+                    value.Fill(getattr(row, name), weight)
+                else:
+                    value.Fill(getattr(row, name))
+
+
+    def process(self):
+        for i, row in enumerate(self.tree):
+            print i
+            #if not self.preselection(row):
+            #    continue
+
+            self.fill_histos(self.histograms, "test", row)
+
+
+    def finish(self):
+        self.write_histos()
+
+
+    def preselection(self, row):
+        for i in xrange(4):
+            if not selections.muSelection(row, 'm' + str(i+1)):
+                return False
+
+        pts = sorted([row.m1Pt, row.m2Pt, row.m3Pt, row.m4Pt])
+        if not (pts[-1] > 20.0 and pts[-2] > 10.0):
+            return False
+
+        return True
---
+++ dblh/DblHBaseSelections.py
@@ -0,0 +1,46 @@
+"""
+Common selections used in DblH
+"""
+
+from FinalStateAnalysis.PlotTools.decorators import memo
+
+
+@memo
+def getVar(name, var):
+    return name + var
+
+
+def muSelection(row, name):
+    if not getattr(row, getVar(name, 'IsGlobal')):
+        return False
+    if getattr(row, getVar(name, 'Pt')) < 5.0:
+        return False
+    if getattr(row, getVar(name, 'AbsEta')) > 2.4:
+        return False
+    if getattr(row, getVar(name, 'PixHits')) < 1:
+        return False
+    if getattr(row, getVar(name, 'GlbTrkHits')) <= 10:
+        return False
+    if getattr(row, getVar(name, 'NormTrkChi2')) >= 10.0:
+        return False
+    if getattr(row, getVar(name, 'TkLayersWithMeasurement')) <= 8:
+        return False
+    if getattr(row, getVar(name, 'MuonHits')) < 1:
+        return False
+    if getattr(row, getVar(name, 'MatchedStations')) < 2:
+        return False
+    if getattr(row, getVar(name, 'PVDXY')) > 2.0:
+        return False
+    if getattr(row, getVar(name, 'IP3DS')) >= 4.0:
+        return False
+
+
+def eleSelection(row, name):
+    if getattr(row, getVar(name, 'Pt')) < 15.0:
+        return False
+    if getattr(row, getVar(name, 'AbsEta')) > 2.5:
+        return False
+    if getattr(row, getVar(name, 'PVDXY')) > 2.0:
+        return False
+    if getattr(row, getVar(name, 'IP3DS')) >= 4.0:
+        return False
